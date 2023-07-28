950e3f3c8daf04fc0dc3f436a28f215ad1f6dfa4
VishalBhuse <vbhuse1997@gmail.com>
2023-07-28 17:39:10 +0530

    css add

--- src/Component/Navbar/Navbar.tsx
+++ src/Component/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
   const location = useLocation();
 
   return (
-    <div className="grid place-items-center bg-blue-400 text-white shadow-xl  h-[5rem] text-center w-[100%]">
+    <div className="grid place-items-center bg-blue-400 text-white shadow-xl  h-[5rem] text-center w-[100%] sticky top-0">
       <h1 className="text-2xl font-bold capitalize">
         {location.pathname.substring(1) || "Contact"}
       </h1>
